027ee754add030663882805b31c988835a885e35
Hugo680 <hugo.poilly@edu.ece.fr>
2024-11-24 23:24:26 +0100

    Create deplacements.h

---
+++ deplacements.h
@@ -0,0 +1,9 @@
+#ifndef DEPLACEMENTS_H
+#define DEPLACEMENTS_H
+#define TAILLE_PLATEAU 18
+
+int deplacement();
+int deplacerPion(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int *x1, int *y1, int direction);
+
+#endif //DEPLACEMENTS_H
+
