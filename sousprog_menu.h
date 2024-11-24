c046994c38e1287d7e3b3ea380a7924602bfab0a
L-pneuh <github.backtalk935@passmail.net>
2024-11-24 17:02:34 +0100

    Update sousprog_menu.h

--- sousprog_menu.h
+++ sousprog_menu.h
@@ -6,7 +6,15 @@
 #define INFOJEU_H
 #define L 18
 #define C 18
+#define TAILLE_PLATEAU 9
 
+typedef struct {
+    char nom[50];
+    int position[2];
+    int nbBarrieres;
+} Joueur;
+
+void sauvegarderPartie(Joueur joueurs[], int nbJoueurs, char plateau[TAILLE_PLATEAU][TAILLE_PLATEAU]);
 
 void infosPrep();
 void quitterJeu();
