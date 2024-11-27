cbd563286ec032332bd4627e66a348b30eb7ffcb
Hugo680 <hugo.poilly@edu.ece.fr>
2024-11-27 14:45:24 +0100

    Update sousprog_menu.c

--- sousprog_menu.c
+++ sousprog_menu.c
@@ -1,14 +1,35 @@
-//
-// Created by lily on 03/11/2024.
-//
-
 #include <stdio.h>
-#include "sousprog_menu.h"
-#include <stdlib.h>
 #include "affichage.h"
+#include "hasard.h"
+#include "sousMenu.h"
 #include "joueurs.h"
+#include "barriere.h"
+#include "deplacements.h"
+#include <stdlib.h>
 
-void calculerScore(Joueur *joueur, int pointsGagnes) {
+void afficherAide() {
+    printf("*************************************************\n");
+    printf("             Regles du jeu Quoridor            \n");
+    printf("*************************************************\n");
+    printf("Le but du jeu est d'etre le premier a atteindre \n");
+    printf("la ligne opposee de son cote du plateau, tout en \n");
+    printf("bloquant les autres joueurs a l'aide de barrieres.\n\n");
+    printf("1. Chaque joueur commence sur la ligne centrale. \n");
+    printf("2. Un joueur peut deplacer son pion d'une case vers \n");
+    printf("   le haut, le bas, la gauche ou la droite.\n");
+    printf("3. A chaque tour, un joueur peut soit deplacer son \n");
+    printf("   pion, soit poser une barriere.\n");
+    printf("4. Une barriere bloque une case, mais elle ne peut \n");
+    printf("   pas etre placee sur une case deja occupee par un \n");
+    printf("   joueur ou une autre barriere.\n");
+    printf("5. Un joueur perd la partie si son pion est bloque \n");
+    printf("   par les barrieres et ne peut plus avancer.\n\n");
+    printf("*************************************************\n");
+    printf("     Bonne chance et amusez-vous bien !\n");
+    printf("*************************************************\n");
+}
+
+/*void calculerScore(Joueur *joueur, int pointsGagnes) {
     joueur->score += pointsGagnes;
     printf("Score mis à jour pour %s : %d points\n", joueur->pseudo, joueur->score);
 }
@@ -18,7 +39,7 @@ void sauvegarderScoreDansFichier(FILE *file, Joueur *joueurs, int m) {
         fprintf(file, "Pseudo : %s, Score : %d\n", joueurs[i].pseudo, joueurs[i].score);
     }
 }
-
+*/
 void quitterJeu() {
     int s;
     printf ("Etes-vous sur de vouloir quitter? Si oui tapez 1 sinon tapez 0 :\n");
@@ -33,35 +54,7 @@ void quitterJeu() {
     }
 }
 
-void nouvellePartie() {
-    char tabInitial[L][C];
-    int m;
-    // Demander à nouveau à l'utilisateur de choisir le nombre de joueurs (2 ou 4)
-    do {
-        printf("Entrez le nombre de joueurs (2 ou 4) : ");
-        scanf("%d", &m);
-    } while (m != 2 && m != 4);  // Boucle tant que l'entrée n'est pas 2 ou 4
-
-    // Initialisation des joueurs
-    declarationJoueur(joueurs, m);
-
-    initialisationPlateau(tabInitial);
-
-    struct Joueur joueur;
-    if (m == 2) {
-        tabInitial[9][1]=joueurs[0].pion;
-        tabInitial[9][17]=joueurs[1].pion;
-    }
-    else if (m == 4) {
-        tabInitial[9][1]=joueurs[0].pion;
-        tabInitial[9][17]=joueurs[1].pion;
-        tabInitial[1][9]=joueurs[2].pion;
-        tabInitial[17][9]=joueurs[3].pion;
-    }
-    afficherPlateau(tabInitial);
-}
-
-void sauvegarderPartie(Joueur joueurs[], int m, char plateau[C][L]) {
+/*void sauvegarderPartie(Joueur joueurs[], int m, char plateau[C][L]) {
     FILE *fichier = fopen("sauvegarde_partie.txt", "w");  // Ouvre le fichier en mode écriture
     if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier de sauvegarde.\n");
@@ -87,25 +80,4 @@ void sauvegarderPartie(Joueur joueurs[], int m, char plateau[C][L]) {
     fclose(fichier);  // Ferme le fichier après l'écriture
     printf("Partie sauvegardee avec succes !\n");
 }
-
-void afficherAide() {
-    printf("*************************************************\n");
-    printf("             Regles du jeu Quoridor            \n");
-    printf("*************************************************\n");
-    printf("Le but du jeu est d'etre le premier a atteindre \n");
-    printf("la ligne opposee de son cote du plateau, tout en \n");
-    printf("bloquant les autres joueurs a l'aide de barrieres.\n\n");
-    printf("1. Chaque joueur commence sur la ligne centrale. \n");
-    printf("2. Un joueur peut deplacer son pion d'une case vers \n");
-    printf("   le haut, le bas, la gauche ou la droite.\n");
-    printf("3. A chaque tour, un joueur peut soit deplacer son \n");
-    printf("   pion, soit poser une barriere.\n");
-    printf("4. Une barriere bloque une case, mais elle ne peut \n");
-    printf("   pas etre placee sur une case deja occupee par un \n");
-    printf("   joueur ou une autre barriere.\n");
-    printf("5. Un joueur perd la partie si son pion est bloque \n");
-    printf("   par les barrieres et ne peut plus avancer.\n\n");
-    printf("*************************************************\n");
-    printf("     Bonne chance et amusez-vous bien !\n");
-    printf("*************************************************\n");
-}
+*/
