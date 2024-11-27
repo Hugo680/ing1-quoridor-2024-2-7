297915bcc2e938de06f8297079a47ef0cbd5e09a
Hugo680 <hugo.poilly@edu.ece.fr>
2024-11-27 11:18:36 +0100

    Update deplacements.h

--- deplacements.h
+++ deplacements.h
@@ -2,8 +2,17 @@
 #define DEPLACEMENTS_H
 #define TAILLE_PLATEAU 18
 
-int deplacement();
-int deplacerPion(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int *x1, int *y1, int direction);
+int deplacement1();
+int deplacerPion1(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int *x1, int *y1, int direction);
+
+int deplacement2();
+int deplacerPion2(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int *x2, int *y2, int direction);
+
+int deplacement3();
+int deplacerPion3(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int *x3, int *y3, int direction);
+
+int deplacement4();
+int deplacerPion4(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int *x4, int *y4, int direction);
 
 #endif //DEPLACEMENTS_H
 
