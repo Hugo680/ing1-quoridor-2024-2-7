555217a9fa9f09963a63e403881a0a0d9943c29e
Hugo680 <hugo.poilly@edu.ece.fr>
2024-11-27 14:47:53 +0100

    Update barriere.h

--- barriere.h
+++ barriere.h
@@ -3,12 +3,20 @@
 #define TAILLE_PLATEAU 18
 #define MAX_BARRIERES 10
 #define barriere_V 186
-#define barriere_H 205
+#define barriere_H 135
 #include "affichage.h"
 
-int barrieres();
-int placer_barriere(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int xx1, int yy1, int xx2, int yy2, int *barrieres_restantes, char tabInitial[L][C],int barrieres);
+int barrieres1();
+int placer_barriere1(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int xx, int yy, int *barrieres_restantes);
 
+int barrieres2();
+int placer_barriere2(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int xx2, int yy2, int *barrieres_restantes);
+
+int barrieres3();
+int placer_barriere3(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int xx3, int yy3, int *barrieres_restantes);
+
+int barrieres4();
+int placer_barriere4(int plateau[TAILLE_PLATEAU][TAILLE_PLATEAU], int xx4, int yy4, int *barrieres_restantes);
 
 
 #endif//BARRIERE_H
