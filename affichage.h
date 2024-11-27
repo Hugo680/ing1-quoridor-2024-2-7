72bf033a16e02186b99b6ed741a6517f25e9f844
Hugo680 <hugo.poilly@edu.ece.fr>
2024-11-27 11:16:40 +0100

    Update affichage.h

--- affichage.h
+++ affichage.h
@@ -1,17 +1,20 @@
-//
-// Created by lily on 02/11/2024.
-//
-
 #ifndef AFFICHAGE_H
 #define AFFICHAGE_H
 
 #define L 18
 #define C 18
 
-#define espaceVideH 196
-#define espaceVideV 179
-#define barriereV 186
-#define barriereH 205
+#define X1 1
+#define X2 17
+#define X3 9
+#define X4 9
+#define Y1 9
+#define Y2 9
+#define Y3 1
+#define Y4 17
+
+#define barriereH 196
+#define barriereV 179
 #define pion1 1
 #define pion2 2
 #define pion3 3
@@ -20,6 +23,13 @@
 void initialisationPlateau(char tabInitial[L][C]);
 void afficherPlateau(char tabPlateau[L][C]);
 void afficherInfosJoueur();
+int scoreFichier();
+void menuJoueurs1();
+void menuJoueurs2();
+void menuJoueurs3();
+void menuJoueurs4();
+void quitterJeu();
+void placerBarriere();
 
 
 
