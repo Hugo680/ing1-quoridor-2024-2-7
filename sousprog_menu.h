b343f5ae6d135240169f0d5a45722e91530b2843
Hugo680 <hugo.poilly@edu.ece.fr>
2024-11-27 14:45:57 +0100

    Update sousprog_menu.h

--- sousprog_menu.h
+++ sousprog_menu.h
@@ -1,15 +1,11 @@
-//
-// Created by lily on 03/11/2024.
-//
-
-#ifndef INFOJEU_H
-#define INFOJEU_H
+#ifndef SOUSMENU_H
+#define SOUSMENU_H
 #define L 18
 #define C 18
 
-void calculerScore(Joueur *joueur, int pointsGagnes);
-void sauvegarderScoreDansFichier(FILE *file, Joueur *joueurs, int m);
-void sauvegarderPartie(Joueur joueurs[], int m, char plateau[C][L]);
+//void calculerScore(Joueur *joueur, int pointsGagnes);
+//void sauvegarderScoreDansFichier(FILE *file, Joueur *joueurs, int m);
+//void sauvegarderPartie(Joueur joueurs[], int m, char plateau[C][L]);
 void afficherAide();
 void infosPrep();
 void quitterJeu();
@@ -19,4 +15,4 @@ void afficherAide();
 void saisiePseudo();
 
 
-#endif //INFOJEU_H
+#endif //SOUSMENU_H
